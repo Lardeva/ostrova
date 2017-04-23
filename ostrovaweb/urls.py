6756aa6c49f9cb965b402a14b910cb472756136a
Lardeva <alardeva@gmail.com>
2017-04-23 20:47:37 +0300

    initial

--- ostrovaweb/urls.py
+++ ostrovaweb/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
 
-from ostrovaCalendar.article_views import ArticleAjaxChainedView
+from ostrovaCalendar import ArticleAjaxChainedView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
