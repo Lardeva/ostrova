3926a1b5bc1b0f6029acb5720cb2d3b10642c108
ana_lardeva <ana_lardeva@abv.bg>
2017-06-05 06:24:11 +0300

    .

--- ostrovaweb/wsgi.py
+++ ostrovaweb/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
+import logging
 import os
 
 from cubes.server import create_server
@@ -14,7 +14,7 @@ from cubes.server import read_slicer_config
 from cubes.server.utils import str_to_bool
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-from werkzeug.wsgi import DispatcherMiddleware
+# from werkzeug.wsgi import DispatcherMiddleware
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,7 +38,43 @@ debug = os.environ.get("SLICER_DEBUG")
 if debug and str_to_bool(debug):
     cubes_application.debug = True
 
+class DispatcherMiddleware(object):
+
+    """Allows one to mount middlewares or applications in a WSGI application.
+    This is useful if you want to combine multiple WSGI applications::
+
+        app = DispatcherMiddleware(app, {
+            '/app2':        app2,
+            '/app3':        app3
+        })
+    """
+
+    def __init__(self, app, mounts=None):
+        self.app = app
+        self.mounts = mounts or {}
+
+    def __call__(self, environ, start_response):
+        script = environ.get('PATH_INFO', '')
+        path_info = ''
+        while '/' in script:
+            if script in self.mounts:
+                app = self.mounts[script]
+                break
+            script, last_item = script.rsplit('/', 1)
+            path_info = '/%s%s' % (last_item, path_info)
+            logging.info("**!!**1" + path_info)
+            logging.info("**!!**2" + script)
+        else:
+            app = self.mounts.get(script, self.app)
+        original_script_name = environ.get('SCRIPT_NAME', '')
+        environ['SCRIPT_NAME'] = original_script_name + script
+        environ['PATH_INFO'] = path_info
+        logging.info("**!!**x" + path_info)
+        logging.info("**!!**y" + script)
+
+        return app(environ, start_response)
+
 application = DispatcherMiddleware(
     django_application,
-    { 'cubes_backend':     cubes_application }
+    { '/cubes_backend':     cubes_application }
 )
