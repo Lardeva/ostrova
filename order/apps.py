d3284faf9876e146cc36b5776814571082a4fa4f
Lardeva <alardeva@gmail.com>
2017-07-02 07:28:26 +0300

    paypal fix

--- order/apps.py
+++ order/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from paypal.standard.ipn.signals import valid_ipn_received
+
+from siteorder.siteorder import show_me_the_money
 
 
 class OrderConfig(AppConfig):
     name = 'order'
     verbose_name = 'Поръчки за парти'
+
+    def ready(self):
+        valid_ipn_received.connect(show_me_the_money)
